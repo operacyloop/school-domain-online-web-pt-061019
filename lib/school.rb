6d8de60d5ef24c8e5353b2123334fe48df890d88
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-07-10 20:12:28 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,41 @@
-# code here!
+# create the "model" school (in the domain model context)
+
+class School
+  
+  # Method 1: the school should have a name and a roster
+  
+  def initialize (name) #initialize allows you when ever a school object whenever there is a dot new... looks for initalize (need it there for dot new)
+    @name = name
+    @roster = {}
+  end
+
+  # Method 2: define the roster
+  
+  def roster
+    @roster
+  end
+
+  # Method 3: should be able to add students, using name of student and the grade they're in
+
+   def add_student(name, grade)
+    if @roster[grade] != nil 
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
+
+  # Method 4: roster by grade (?)
+
+   def grade(grade)
+    @roster[grade]
+  end
+
+  # Method 5: get a sorted list
+  
+   def sort
+    @roster.each do |key, value|
+      value.sort!
+    end
+  end
+end 
